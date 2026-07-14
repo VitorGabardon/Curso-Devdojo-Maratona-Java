64ff7caab647ace4d311a2e44f0b2a28a94db0ab
Vitor Gabardon <vitorbonimani72@gmail.com>
2026-07-14 18:08:03 -0300

    Meus estudos com maratona Java

--- src/academy/devdojo/maratonajava/javacore/Csobrecargametodos/dominio/anime.java
+++
@@ -1,4 +0,0 @@
-package academy.devdojo.maratonajava.javacore.Csobrecargametodos.dominio;
-
-public class anime {
-}
---
+++ src/academy/devdojo/maratonajava/javacore/Csobrecargametodos/teste/animeTeste01.java
@@ -0,0 +1,5 @@
+package academy.devdojo.maratonajava.javacore.Csobrecargametodos.teste;
+
+public class animeTeste01 {
+    Anime anime = new Anime();
+}
